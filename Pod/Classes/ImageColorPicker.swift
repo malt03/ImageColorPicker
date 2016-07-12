4932a1cef326652587349acb9c8ad85019384bc8
Koji Murata <koji.murata@dena.com>
2016-07-12 18:29:15 +0900

    crash

--- Pod/Classes/ImageColorPicker.swift
+++ Pod/Classes/ImageColorPicker.swift
@@ -9,72 +9,78 @@
 import UIKit
 
 public class ImageColorPicker {
-  private var pickerBytesPerRow: Int!
-  private var pickerWidth: Int!
-  private var pickerHeight: Int!
-  private var pickerImageCoefficient: Int!
+  private var pickerBytesPerRow: Int?
+  private var pickerWidth: Int?
+  private var pickerHeight: Int?
+  private var pickerImageCoefficient: Int?
   private var pickerPixelBuffer: UnsafeMutablePointer<UInt8>?
   
-  private var initImage: Bool
   private let nilWhenOutside: Bool
   
   public init(nilWhenOutside n: Bool = false) {
-    initImage = false
     nilWhenOutside = n
   }
   
   public init(image: UIImage, nilWhenOutside n: Bool = false) {
-    initImage = true
     nilWhenOutside = n
     setImage(image)
   }
   
   deinit {
-    pickerPixelBuffer?.dealloc(pickerWidth * pickerHeight * 4)
+    guard let buffer = pickerPixelBuffer,
+      width = pickerWidth,
+      height = pickerHeight else { return }
+    buffer.dealloc(width * height * 4)
   }
   
   public func setImage(image: UIImage?) {
     guard let i = image else {
-      initImage = false
       return
     }
     
-    pickerPixelBuffer?.dealloc(pickerWidth * pickerHeight * 4)
+    if let buffer = pickerPixelBuffer, width = pickerWidth, height = pickerHeight {
+      buffer.dealloc(width * height * 4)
+    }
     
     let imageRef = i.CGImage
-    pickerWidth = CGImageGetWidth(imageRef)
-    pickerHeight = CGImageGetHeight(imageRef)
-    pickerImageCoefficient = Int(CGFloat(pickerWidth) / i.size.width)
-    pickerBytesPerRow = pickerWidth * 4
+    let width = CGImageGetWidth(imageRef)
+    let height = CGImageGetHeight(imageRef)
+    pickerWidth = width
+    pickerHeight = height
+    pickerImageCoefficient = Int(CGFloat(pickerWidth!) / i.size.width)
+    pickerBytesPerRow = pickerWidth! * 4
     
-    pickerPixelBuffer = UnsafeMutablePointer<UInt8>.alloc(pickerWidth * pickerHeight * 4)
+    pickerPixelBuffer = UnsafeMutablePointer<UInt8>.alloc(width * height * 4)
     let colorSpace = CGColorSpaceCreateDeviceRGB()
     let option = CGBitmapInfo.ByteOrder32Little.rawValue | CGImageAlphaInfo.PremultipliedLast.rawValue
-    let context = CGBitmapContextCreate(pickerPixelBuffer!, pickerWidth, pickerHeight, 8, pickerWidth * 4, colorSpace, option)
+    let context = CGBitmapContextCreate(pickerPixelBuffer!, width, height, 8, width * 4, colorSpace, option)
     CGContextSetBlendMode(context, .Copy)
-    CGContextDrawImage(context, CGRect(origin: .zero, size: CGSize(width: pickerWidth, height: pickerHeight)), imageRef)
-    
-    initImage = true
+    CGContextDrawImage(context, CGRect(origin: .zero, size: CGSize(width: width, height: height)), imageRef)
   }
   
   public func pick(inout point: CGPoint) -> UIColor? {
-    var x = Int(point.x) * pickerImageCoefficient
-    var y = Int(point.y) * pickerImageCoefficient
+    guard let buffer = pickerPixelBuffer,
+      coefficient = pickerImageCoefficient,
+      bytesPerRow = pickerBytesPerRow,
+      width = pickerWidth,
+      height = pickerHeight else { return nil }
+    var x = Int(point.x) * coefficient
+    var y = Int(point.y) * coefficient
     
     if nilWhenOutside {
-      if x < 0 || pickerWidth - 1 < x || y < 0 || pickerHeight - 1 < y { return nil }
+      if x < 0 || width - 1 < x || y < 0 || height - 1 < y { return nil }
     } else {
-      if x < 0 { x = 0 } else if pickerWidth - 1 < x { x = pickerWidth - 1 }
-      if y < 0 { y = 0 } else if pickerHeight - 1 < y { y = pickerHeight - 1 }
-      point = CGPoint(x: x / pickerImageCoefficient, y: y / pickerImageCoefficient)
+      if x < 0 { x = 0 } else if width - 1 < x { x = width - 1 }
+      if y < 0 { y = 0 } else if height - 1 < y { y = height - 1 }
+      point = CGPoint(x: x / coefficient, y: y / coefficient)
     }
     
-    let pixelInfo = pickerBytesPerRow * y + (x * 4)
+    let pixelInfo = bytesPerRow * y + (x * 4)
     
-    let a = CGFloat(pickerPixelBuffer![pixelInfo+0]) / CGFloat(255.0)
-    let b = CGFloat(pickerPixelBuffer![pixelInfo+1]) / CGFloat(255.0)
-    let g = CGFloat(pickerPixelBuffer![pixelInfo+2]) / CGFloat(255.0)
-    let r = CGFloat(pickerPixelBuffer![pixelInfo+3]) / CGFloat(255.0)
+    let a = CGFloat(buffer[pixelInfo+0]) / CGFloat(255.0)
+    let b = CGFloat(buffer[pixelInfo+1]) / CGFloat(255.0)
+    let g = CGFloat(buffer[pixelInfo+2]) / CGFloat(255.0)
+    let r = CGFloat(buffer[pixelInfo+3]) / CGFloat(255.0)
     
     return UIColor(red: r, green: g, blue: b, alpha: a)
   }
